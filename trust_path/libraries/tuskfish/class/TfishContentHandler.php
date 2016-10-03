7b5bf7619ea2d86d389eae5982c1310bc21b9a0d
Simon <Simon@Hyperion>
2016-10-03 08:12:16 +0700

    * Removed output escaping on plain text form elements (admin side data entry and edit only). * Retained full output escaping on user-side display of all data fields.

--- trust_path/libraries/tuskfish/class/TfishContentHandler.php
+++ trust_path/libraries/tuskfish/class/TfishContentHandler.php
@@ -641,7 +641,7 @@ class TfishContentHandler
 		
 		// Populate the object from the $row using whitelisted properties.
 		if ($content_object) {
-			$content_object->loadProperties($row);
+			$content_object->loadProperties($row, true);
 			
 			// Populate the tag property.
 			if (isset($content_object->tags) && !empty($content_object->id)) {
