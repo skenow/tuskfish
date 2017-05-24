6b53da30855b184bfd5cb6965079ee54f7718716
Simon <Simon@192.168.1.107>
2017-05-24 22:00:21 +0700

    * Adding to skeleton of TfishCache class

--- trust_path/libraries/tuskfish/class/TfishCache.php
+++ trust_path/libraries/tuskfish/class/TfishCache.php
@@ -6,7 +6,25 @@
 * Handles cache operations - checking if a cached versions of pages exist, generating them if they
 * don't, clearing the cache when asked. Works pretty much the same way the image caching system
 * does, but the image cache will be a subsiduary of this system. Need to build it into the existing
-* output buffering and rendering framework somehow. 
+* output buffering and rendering framework somehow.
+ * 
+ * Some principles:
+ * 
+ * 1. Cache must come after all the header setup (autoload, error handler, connect to database, 
+ * reading preferences and starting the session, admin check and site closed check. It *might* be
+ * able to come before the language file, metadata and template assignment but this needs to be 
+ * tested. Basically it would just be easier to run the cache check at the end of the header, for 
+ * the time being.
+ * 
+ * 2. Calls to the cache directory need to be locked down to prevent directory traversals and similar
+ * shenannigans. It would be best to construct URLs from the parameters passed in, rather than to
+ * draw on $_SERVER['REQUEST_URL'], as all the data filtering is already in place. Cache calls for
+ * individual content objects can simply be based on ID, nothing else is needed. Index pages are a
+ * bit more complicated in that there can be multiple parameters.
+ * 
+ * 3. Cache timeout should be based on a preference. A minimum value should be enforced in order to
+ * prevent admins from setting stupid ones. There should also be an on/off control, and a 'flush'
+ * link in the control panel.
 *
 * @copyright	Simon Wilkinson (Crushdepth) 2017
 * @license		https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GNU General Public License (GPL) V2
@@ -29,7 +47,6 @@ class TfishFileHandler extends TfishAncestralObject
 		 */
 		$this->__properties['cache_life'] = 'int'; 
 		// TFISH_CACHE_PATH and TFISH_CACHE_URL are available (defined in config.php).
-		// Time to expire (should be a preference but let's worry about that later).
 		
 		/**
 		 * Set the permitted properties of this object.
@@ -45,22 +62,30 @@ class TfishFileHandler extends TfishAncestralObject
 		$this->__data['cache_life'] = 86400; // 24 hours, for testing purpses.
 	}
 
-	// Check if a cached version of the page exists.
-	// What file name convention should be used?
-	public static function checkCache() {
-
+	/**
+	 * 
+	 * 
+	 * @param int $id
+	 */
+	public static function checkCache($id) {
+		
 	}
 
-	// If not, generate a cached version.
-	// What file name convention should be used?
+	/**
+	 * Save a copy of this page to the cache directory.
+	 * 
+	 */
 	public static function cachePage() {
 
 	}
 
-	// Clear the cache directory (only). Need to lock the file path down hard (see TfishFileHandler).
-	// Torch everything, or a specific file. The latter would be useful
-	// if you are just editing a particular object.
-	public static function clearCache() {
+	/**
+	 * Clear the cache, or optionally a single file if ID parameter supplied.
+	 * 
+	 * @param int $id
+	 * @return boolean
+	 */
+	public static function clearCache($id = false) {
 		$result = self::_clearCache();
 		if (!$result) {
 			trigger_error(TFISH_ERROR_FAILED_TO_DELETE_DIRECTORY, E_USER_NOTICE);
