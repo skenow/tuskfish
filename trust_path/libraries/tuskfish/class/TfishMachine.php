2041f35b2c23eca58a3aa9785ffba0e6aa528796
Simon <Simon@192.168.1.107>
2017-06-04 11:30:32 +0700

    * Skeleton of machine handler class.

---
+++ trust_path/libraries/tuskfish/class/TfishMachine.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * Base Tuskfish machine object class.
+ * 
+ * Represents a networked computer, sensor or IOT device with which Tuskfish can communicate,
+ * either to send instructions and data or to receive them. This class is intended to facilitate
+ * interaction between Tuskfish and the IOT, for example to log data or to manage a group of
+ * sensors or devices.
+ * 
+ * Authenticity and integrity of communications is assured through use of HMACs with private keys
+ * shared by Tuskfish and remote machines, however they are not private (encrypted). Do not use
+ * this interface to transmit confidential information unless you secure the transport layer. 
+ *
+ * @copyright	Simon Wilkinson (Crushdepth) 2017
+ * @license		https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GNU General Public License (GPL) V2
+ * @since		1.0
+ * @author		Simon Wilkinson (Crushdepth) <simon@isengard.biz>
+ * @package		core
+ */
+
+/**
+ * Requirements
+ * 
+ * 1. Shared secret key for each device, needs to be stored in machine database table.
+ * 2. HMAC authentication and integrity check.
+ * 3. Vocabulary (whitelist) of commands that can be sent.
+ * 4. Vocabulary (whitelist) of commands that can be received.
+ * 5. Authority file for each device - which commands is it allowed to use?
+ * 6. Each command should probably have a separate method, to keep the control script clean.
+ * 7. TfishMachineHandler class to handle the actual HMAC and signalling.
+ */
+
+if (!defined("TFISH_ROOT_PATH"))
+    die("TFISH_ERROR_ROOT_PATH_NOT_DEFINED");
+
+class TfishMachine extends TfishTfishAncestralObject
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        
+        /**
+         * Whitelist of allowed properties and data types.
+         */
+        $this->__properties['id'] = 'int'; // Auto-increment, set by database.
+        $this->__properties['title'] = 'string'; // Human readable name of this machine.
+        $this->__properties['url'] = 'url'; // The URL and port of the remote device.
+        $this->__properties['shared_key'] = 'string'; // Used for HMAC authentication / integrity checks
+        $this->__properties['allowed_in'] = 'array'; // Must check contents of array against whitelist.
+        $this->__properties['allowed_out'] = 'array'; // Must check contents of array against whitelist.
+        $this->__properties['request_counter'] = 'array'; // Track the number of requests to mitigate replay attacks.
+        $this->__properties['enabled'] = 'int'; // Permit or deny communications with this machine.
+        
+        /**
+         * Set the permitted properties of this object.
+         */
+        foreach ($this->__properties as $key => $value) {
+            $this->__data[$key] = '';
+        }
+
+        /**
+         * Set default values of permitted properties.
+         */
+        // Machine communications are disabled by default. You must explicitly enable them to talk
+        // to a remote device, for security reasons.
+        $this->__data['enabled'] = 0;
+    }
+    
+    /**
+     * Set the value of an object property and will not allow non-whitelisted properties to be set.
+     * 
+     * Intercepts direct calls to set the value of an object property. This method is overridden by
+     * child classes to impose data type restrictions and range checks before allowing the property
+     * to be set. Tuskfish objects are designed not to trust other components; each conducts its
+     * own internal validation checks. 
+     * 
+     * @param string $property name
+     * @param return void
+     */
+    public function __set($property, $value)
+    {
+        if (isset($this->__data[$property])) {
+
+            // Validate $value against expected data type and business rules
+            $type = $this->__properties[$property];
+            switch ($type) {
+                
+                // allowed_in, allowed_out
+                case "array":
+                    if ($property == 'allowed_in') {
+                        // Whitelist of allowed inbound commands issued by remote machines.
+                    }
+                    if ($property == 'allowed_out') {
+                        // Whitelist of allowed outbound commands to remote machines.
+                    }
+                    break;
+                
+                // id, enabled, request_counter.
+                case "int":
+                    break;                
+                
+                // title, shared_key.
+                case "string":
+                    break;
+            }
+        }
+    }
+
+}
---
+++ trust_path/libraries/tuskfish/class/TfishMachineHandler.php
@@ -0,0 +1,222 @@
+<?php
+
+/**
+ * Tuskfish machine object handler class.
+ * 
+ * Handles interaction with remote machines and IOT devices, including authentication and integrity
+ * checks via HMAC.
+ * 
+ * Commands are authenticated via calculation of SHA256 HMAC with preshared, per-device keys. 
+ * This allows the integrity and authenticity of the request to be validated, but the request is not
+ * encrypted (ie. it is not private). Replay attacks are guarded against by inclusion of a counter
+ * (tracked by the server), timestamp and a random string of crap (RSOC) in the the request, which 
+ * ensures that each request has a unique HMAC fingerprint.
+ * 
+ * However, some devices (eg. Arduino) may not be capable of supplying all of these credentials due
+ * to resource limitations, so the challenge tests are arranged in a semi-independent manner; 
+ * you can comment out particular tests if your remote device can't handle them. Of course, this 
+ * reduces security. As a minimum: Use the client_id, HMAC and counter or timestamp. The random 
+ * text is highly desirable. If your client device can't generate adequate randomness, consider 
+ * pre-generating a large chunk of it it on a more capable device and storing it on the client (in
+ * this case, you MUST discard random data as you use it. NEVER re-use it).
+ * 
+ * Key length: Use a 256 byte key for optimum security. Shorter keys weaken security. There is 
+ * no evidence that longer keys improve it. 256 is what the algorithm uses internally.
+ * 
+ * Randomness: Get your key from a high quality random source, such as GRC's perfect passwords page.
+ * Be aware that random number generation on small devices (notably Arduino) is often of poor
+ * quality and unacceptable for security purposes. https://www.grc.com/passwords.htm
+ * 
+ * Data length: The overall length of device-supplied data used to calculate the HMAC should be at 
+ * least 256 characters for security reasons. So you may want to adjust the length of your RSOC
+ * accordingly.
+ *
+ * @copyright   Simon Wilkinson (Crushdepth) 2013-2017
+ * @license     https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GNU General Public License (GPL) V2
+ * @since       1.0
+ * @author      Simon Wilkinson (Crushdepth) <simon@isengard.biz>
+ * @package     core
+ */
+if (!defined("TFISH_ROOT_PATH"))
+    die("TFISH_ERROR_ROOT_PATH_NOT_DEFINED");
+
+class TfishContentHandler
+{
+
+    function __construct()
+    {
+        
+    }
+
+    // Check the authenticity and integrity of a signal.
+    protected static function authenticate($dirty_signal)
+    {
+		$this->validateData();
+		$this->checkAuthorised();
+		$this->checkExpired();
+		$this->checkCounter();
+		$this->checkHmac();
+		$this->authenticated = $this->check_sanity();
+		if ($this->authenticated == true) {
+			$this->executeCommand($clean_command);
+		}
+    }
+
+    // Check if a machine is authorised to execute a particular command.
+    protected static function checkAuthorised($id, $command)
+    {
+        
+    }
+    
+    // Determines if a request timestamp has expired.
+    protected static function checkExpired($timestamp) {
+        
+    }
+    
+    // Checks if a request has expired based on the machine's counter, to guard against replay attacks.
+    protected static function checkRequestCounter($id, $counter)
+    {
+        
+    }
+
+    
+    // Execute a whitelisted command. Should only be called after authentication passed.
+    protected static function executeCommand()
+    {
+        
+    }
+
+    // Count the number of machines matching $criteria.
+    protected static function getCount($criteria)
+    {
+        
+    }
+    
+    /**
+	 * Checks if the remote command HMAC matches that calculated by Tuskfish using pre-shared key
+	 * 
+	 * The remote device supplies a HMAC of the command parameters calculated using a pre-shared key.
+	 * Straylight also calculates the HMAC locally. If the HMACs match, the integrity and
+     * authenticity of the remote command is validated and Tuskfish will execute it. If the HMACs
+     * do not match, then the command is discarded as invalid.
+	 * 
+	 * Note that in order for the HMACs to match, the remote client must use EXACTLY the same data 
+	 * in exactly the same order as the Straylight module when calculating the HMAC. You can get the
+	 * order from the function below.
+	 */
+    protected static function checkHmac()
+    {
+        /**
+         * HMAC calculation procedure (must be followed by both Tuskfish and remote device).
+         * 
+         * 1. Retrieve the shared key.
+         * 2. Concatenate signal parameters ($data) in the following order:
+         *    - client_id
+         *    - command
+         *    - request counter
+         *    - request timestamp
+         *    - random string of crap (RSOC)
+         * 
+         * 3. Calculate sha256 hash:
+         * 
+         * $local_hash = hash_hmac('sha256', $data, $key, false);
+         * 
+         * 4. Extract the first 32 characters (only) of the hash and compare to validate.
+         * 
+         * $local_hash = mb_substr($hash, 0, 32, "UTF-8");
+         * 
+         * if ($local_hash == $remote_hash) {
+         *     // Good signal: Authenticity and integrity of communication established.
+         * } else {
+         *     // Bad signal: May be spoofed or modified, discard.
+         * }
+         * 
+         * Note that you still need to check that a machine is enabled and authorised to use a
+         * particular command before acting on it.
+         * 
+         */
+    }
+
+    // Return an array of machine IDs and names.
+    protected static function getList($criteria)
+    {
+        
+    }
+
+    // Return a single machine object specified by ID.
+    protected static function getMachine($id)
+    {
+        
+    }
+
+    // Return multiple machine objects matching $criteria.
+    protected static function getMachines($criteria)
+    {
+        
+    }
+
+    // Return a whitelist of permitted commands.
+    protected static function getVocabulary()
+    {
+        return array(
+            'closeSite', // Close website.
+            'enableCache', // Enable website cache.
+            'disableCache', // Disable website cache.
+            'flushCache', // Flush website cache.
+            'openSite', // Open website.            
+        );
+    }
+
+    // Insert a new machine object.
+    protected static function insert()
+    {
+        
+    }
+
+    // Enable or disable a machine.
+    protected static function enable($flag)
+    {
+        
+    }
+
+    // Update a machine object specified by ID.
+    protected static function update($id)
+    {
+        
+    }
+    
+    protected static function updateRequestCounter($id)
+    {
+        
+    }
+    
+    protected static function validateData()
+    {
+        
+    }
+
+    /////////////////////////////////////
+    ////////// Command methods //////////
+    /////////////////////////////////////
+    
+    protected  function closeSite()
+    {
+        
+    }
+    
+    protected function disableCache()
+    {
+        
+    }
+    
+    protected function flushCache()
+    {
+        
+    }
+    
+    protected function openSite()
+    {
+        
+    }
+        
+}
