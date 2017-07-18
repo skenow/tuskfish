4efae541533df6534c351b95ce160e6d3f274346
Simon <Simon@communications>
2017-07-18 12:46:06 +0700

    * Disable display of errors on screen. * Enable logging of errors to file.

--- trust_path/libraries/tuskfish/tfish_header.php
+++ trust_path/libraries/tuskfish/tfish_header.php
@@ -24,6 +24,8 @@ header('Content-Type: text/html; charset=utf-8');
 require_once TFISH_LIBRARIES_PATH . 'htmlpurifier/library/HTMLPurifier.auto.php';
 
 // Set error reporting levels and custom error handler.
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
 error_reporting(E_ALL & ~E_NOTICE);
 set_error_handler("TfishLogger::logErrors");
 
