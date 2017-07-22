3f91da536a7d649b16a6f3e1cce2a9d83f59cd37
Simon <Simon@192.168.1.106>
2017-07-22 13:29:31 +0700

    * Change password page back to admin theme.

--- admin/password.php
+++ admin/password.php
@@ -16,7 +16,7 @@ require_once "../mainfile.php";
 require_once TFISH_ADMIN_PATH . "tfish_admin_header.php";
 
 // Specify theme set, otherwise 'default' will be used.
-$tfish_template->setTheme('sb_admin');
+$tfish_template->setTheme('admin');
 
 // Validate input parameters. Note that passwords are not sanitised in any way.
 $op = isset($_REQUEST['op']) ? TfishFilter::trimString($_REQUEST['op']) : false;
