3261734593ed6c72a2ed4d492633b4079fe85e42
Simon <Simon@192.168.1.109>
2018-08-17 16:00:19 +0700

    * Create base file structure for Machines module.

---
+++ trust_path/modules/machines/language/english.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Tuskfish SOMEMODULE language constants (English).
+ * 
+ * Translate this file to convert the SOMEMODULE module to another language.
+ *
+ * @copyright   Your Name 2018+ (https://tuskfish.biz)
+ * @license     https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GNU General Public License (GPL) V2
+ * @author      Your Name <you@email.com>
+ * @since       1.0
+ * @package     language
+ */
+declare(strict_types=1);
+
+// Divide your constants into logical sections separated by comments
+define("TFISH_MACHINE_MACHINES", "Machines");
