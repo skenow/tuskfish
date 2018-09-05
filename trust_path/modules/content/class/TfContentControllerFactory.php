6dd2b43c39c2dcbd28c5aa121da9a65e108c04ec
Simon <Simon@192.168.1.109>
2018-09-05 10:36:19 +0700

    * Add missing descriptin to TfControllerFactory class.

--- trust_path/modules/content/class/TfContentControllerFactory.php
+++ trust_path/modules/content/class/TfContentControllerFactory.php
@@ -13,6 +13,8 @@
 declare(strict_types=1);
 if (!defined("TFISH_ROOT_PATH")) die("TFISH_ERROR_ROOT_PATH_NOT_DEFINED");
 /**
+ * Handles instantiation of controllers for content objects.
+ * 
  * @copyright   Simon Wilkinson 2013+ (https://tuskfish.biz)
  * @license     https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GNU General Public License (GPL) V2
  * @author      Simon Wilkinson <simon@isengard.biz>
