f5b7737e0a94b6c6e6d65aa8502ac9e9cfcc8ad3
Simon <Simon@192.168.1.109>
2018-11-23 13:18:12 +0700

    * Removed session_cache_limiter from tfHeader.php, it was too much of a blunt instrument. However, have retained it on the search.php page where it is most useful.

--- trust_path/libraries/tuskfish/tfHeader.php
+++ trust_path/libraries/tuskfish/tfHeader.php
@@ -33,9 +33,6 @@ ini_set('display_errors', '0');
 ini_set('log_errors', '1');
 error_reporting(E_ALL);
 
-// Set cache limiter to avoid 'Document expired' errors on forms when clicking the back button in a browser.
-//session_cache_limiter('private_no_expire');
-
 // Make core language files available.
 include TFISH_DEFAULT_LANGUAGE;
 /**
